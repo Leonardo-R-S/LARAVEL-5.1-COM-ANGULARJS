da922cc5cca80dee9ed837b27b7178441648b30c
Leo e Luana <Leo e Luana>
2017-01-11 17:42:37 -0200

    Segunda fase do curso Laravel 5.1 com Angular - Ativando as seeds Client e Project

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,17 +15,17 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
 //      Call the Seed UserTableSeeder(Chama a seed UserTableSeeder)
-        //$this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
 
 //      Call the Seed ClientTableSeeder(Chama a seed ClientTableSeeder)
-        //$this->call(ClientTableSeeder::class);
+       /$this->call(ClientTableSeeder::class);
 
 
 //      Call the Seed ProjectTableSeeder(Chama a seed ProjectTableSeeder)
-       // $this->call(ProjectTableSeeder::class);
+        $this->call(ProjectTableSeeder::class);
 
 //      Call the Seed ProjectNoteTableSeeder(Chama a seed ProjectNoteTableSeeder)
-        $this->call(ProjectNoteTableSeeder::class);
+        //$this->call(ProjectNoteTableSeeder::class);
 
         Model::reguard();
     }
